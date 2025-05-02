d0bddcc577d84a30a210d301ca64950e78459a7a
Lilianroux <lilianroux02@gmail.com>
2025-05-02 12:07:15 +0200

    First commit

--- src/test/java/fr/miage/lroux/utilisateur/UtilisateurApplicationTests.java
+++ src/test/java/fr/miage/lroux/utilisateur/UtilisateurApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UtilisateurApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
